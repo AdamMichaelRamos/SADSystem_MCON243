6883da3391a34ba2d95e5da0f32a0c5c6512b71d
Adam Ramos <aramos15@student.touro.edu>
2018-05-27 18:19:31 -0400

    created a saparate module for master

---
+++ SADMaster/src/com/touro/mcon243/sadmaster/Main.java
@@ -0,0 +1,10 @@
+package com.touro.mcon243.sadmaster;
+
+/**
+ * Created by amram on 5/27/2018.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello SADMaster");
+    }
+}
